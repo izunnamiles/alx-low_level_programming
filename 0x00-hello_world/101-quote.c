f68c20dba1abe2981d18015279eb3ab9d317cfe3
izu okafor <izunnavic@gmail.com>
2022-10-13 11:58:10 -0700

    added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: use printf function to return a string
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
