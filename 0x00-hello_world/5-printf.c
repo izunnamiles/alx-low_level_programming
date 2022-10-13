a9639c3ca7464d4c8fe1e5a43bd4eab98c56e58e
izu okafor <izunnavic@gmail.com>
2022-10-13 05:36:05 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: use printf function to return a string
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
