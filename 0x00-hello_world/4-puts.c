bfb5f75bcdc02d3ad78c36abf519e9860b375ab0
izu okafor <izunnavic@gmail.com>
2022-10-13 05:23:05 -0700

    added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * return: always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
