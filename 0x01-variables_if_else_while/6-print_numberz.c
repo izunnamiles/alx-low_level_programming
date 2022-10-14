9fed5c465dfadb87c1eb9ba0de2ac6aeb76b3337
izu okafor <izunnavic@gmail.com>
2022-10-14 11:49:17 -0700

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: conditional statements
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar("%d");
+	}
+	putchar('\n');
+	return (0);
+}
