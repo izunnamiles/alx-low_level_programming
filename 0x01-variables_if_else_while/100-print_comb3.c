a740c056e090c0749ca76a0877c5cc39704bf07c
izu okafor <izuchukwu@seamlesshr.com>
2022-10-14 21:00:13 +0100

    added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: conditional statements
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i,j;
+
+	for (i = '48'; i <= '56'; i++)
+	{
+		for(j = '49'; j <= '57'; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (i != '56' || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
