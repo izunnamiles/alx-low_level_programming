df6b1a5f6d557e07ff57ef765569091a91abfe8f
izu okafor <izunnavic@gmail.com>
2022-10-14 11:18:00 -0700

    added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: conditional statements
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'e' || x == 'q')
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
