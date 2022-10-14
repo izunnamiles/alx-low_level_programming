ffc6737bf82d5c4af61ca6709943659fc2f9c47a
izu okafor <izunnavic@gmail.com>
2022-10-14 11:43:29 -0700

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: conditional statements
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d\n", x);
+	}
+	printf('\n');
+	return (0);
+}
