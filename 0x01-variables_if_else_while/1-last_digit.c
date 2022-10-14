83c0ecafeeacf37c807ef3ebca8c6ba87653022b
izu okafor <izunnavic@gmail.com>
2022-10-14 10:12:44 -0700

    added 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Description: conditional statements
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int lastNum;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastNum = n % 10;
+	if (lastNum > 5)
+		printf("Last digit %d and is greater than 5\n", n);
+	if (lastNum == 0)
+		printf("Last digit %d and is 0\n", n);
+	if (lastNum < 6 && lastNum != 0)
+		printf("Last digit %d and is less than 6 and not 0\n", n);
+	return (0);
+}
