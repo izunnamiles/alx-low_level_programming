74153d448c11268e4de92efec1a6bdcf2c149837
izu okafor <izunnavic@gmail.com>
2022-10-14 12:23:53 -0700

    added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: conditional statements
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	putchar('\n');
+	return (0);
+}
