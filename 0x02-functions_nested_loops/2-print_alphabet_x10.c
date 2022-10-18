228d8dce81f1768320d17e2886a93121b3863757
izu okafor <izuchukwu@seamlesshr.com>
2022-10-18 13:19:40 +0100

    added 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,17 +9,14 @@
 void print_alphabet_x10(void)
 {
   int i;
-  
+  char x;
   i = 0;
   while (i < 10)
   {
-    char x;
-
     for (x = 'a'; x <= 'z'; x++)
     {
       _putchar(x);
     }
-    
     _putchar('\n');
     i++;
   }
