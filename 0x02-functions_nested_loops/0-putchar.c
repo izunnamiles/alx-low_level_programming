21094da48ecce29431c5c12898b10a03c23dca3b
izu okafor <izuchukwu@seamlesshr.com>
2022-10-18 11:25:45 +0100

    added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - entry point
+ *
+ * Description: functions and nested loops
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char x;
+
+	_putchar(x);
+	return (0);
+}
