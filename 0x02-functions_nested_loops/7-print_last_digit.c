c2e85282825480aa17791b8d855a7256ff299ea8
izu okafor <izuchukwu@seamlesshr.com>
2022-10-18 15:04:46 +0100

    added 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ *
+ * Description: checks if parameter is alphabet
+ *
+ * @c: is the expected parameter
+ *
+ * Return: always 0 (success)
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
