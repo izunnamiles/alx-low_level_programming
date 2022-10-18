2fe2983fe9d5b018d85505642e9884784192b52f
izu okafor <izuchukwu@seamlesshr.com>
2022-10-18 14:03:14 +0100

    added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - entry point
+ *
+ * Description: prints signs, 0 or 1  
+ *
+ * @n: is the expected parameter
+ *
+ * Return: always 0 (success)
+ */
+int print_sign(int n)
+{
+	if (n > '0')
+		_putchar('+')
+		return (1);
+	else if (n == '0')
+		_putchar('0')
+		return (0);
+	else
+		_putchar('-')
+		return (-1)
+}
