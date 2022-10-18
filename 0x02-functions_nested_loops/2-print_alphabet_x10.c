aada10a800fbe0346ead791512399e9d6baa53fc
izu okafor <izuchukwu@seamlesshr.com>
2022-10-18 13:15:51 +0100

    added 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - entry point
+ * print_alphabet_x10 - entry point
  *
  * Description: prints a-z x10
  *
@@ -11,13 +11,15 @@ void print_alphabet_x10(void)
   int i;
   
   i = 0;
-  while (i < 10){
+  while (i < 10)
+  {
     char x;
 
     for (x = 'a'; x <= 'z'; x++)
     {
       _putchar(x);
     }
+    
     _putchar('\n');
     i++;
   }
