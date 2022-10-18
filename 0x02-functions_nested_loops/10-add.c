1b649b52a1b067e491ad79762dea3747fa7092cb
izu okafor <izuchukwu@seamlesshr.com>
2022-10-18 16:27:00 +0100

    added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ *
+ * Description: return absolute integer
+ *
+ * @c: is the expected parameter
+ *
+ * Return: always 0 (success)
+ */
+int _abs(int c, int d)
+{
+	return (c + d);
+}
