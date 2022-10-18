99d24d3117331c37d55111ea4f961f4cde374137
izu okafor <izuchukwu@seamlesshr.com>
2022-10-18 15:50:20 +0100

    added 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,19 +4,15 @@
  *
  * Description: return countdown
  *
- * @c: is the expected parameter
- *
  * Return: 24 hour countdown
  */
 void jack_bauer(void)
 {
 	int h,m;
 
-	h = 0;
-	m = 0;
-	while (h < 24)
+	for (h = 0; h < 24; h++)
 	{
-		while (m < 60)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
@@ -24,8 +20,6 @@ void jack_bauer(void)
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar('\n');
-			m++;
 		}
-		h++;
 	}
 }
