8f10bd3a5de592c66c20bda77ec42fe7b07a354c
izu okafor <izuchukwu@seamlesshr.com>
2022-10-20 12:16:15 +0100

    added 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ *
+ * Description: functions and nested loops
+ *
+ *@c: parameters expects Capital
+ *
+ * Return: returns 1 if it's capital else 0
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
