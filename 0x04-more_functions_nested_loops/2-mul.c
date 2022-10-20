f8040c5a763d544dd0df8faa2473c2359528be1d
izu okafor <izuchukwu@seamlesshr.com>
2022-10-20 13:16:27 +0100

    added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - entry point
+ *
+ * Description: functions and nested loops
+ *
+ * @a: first parameter expects an integer
+ * @b: second parameter expects an integer
+ *
+ * Return: returns a product of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
