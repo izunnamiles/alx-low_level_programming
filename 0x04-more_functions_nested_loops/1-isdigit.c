b8cb58e9dee87d878c3067080c7b14c96acb4fe5
izu okafor <izuchukwu@seamlesshr.com>
2022-10-20 13:07:43 +0100

    added 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ *
+ * Description: functions and nested loops
+ *
+ * @c: parameters expects Capital
+ *
+ * Return: returns 1 if it's capital else 0
+ */
+int _isupper(int c)
+{
+	if ((c <= '9'))
+		return (1);
+	else
+		return (0);
+}
