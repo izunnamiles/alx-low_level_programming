4558c52080f93127182d7d7e583e6ef72e17720f
izu okafor <izuchukwu@seamlesshr.com>
2022-10-20 13:36:11 +0100

    added 3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,11 @@
  */
 void print_numbers(void)
 {
-	int x[] = {0, 1, 2, 3 , 4, 5, 6, 7, 8, 9};
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(x[i]);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
