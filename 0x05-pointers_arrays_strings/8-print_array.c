1c4a553472e9dd072945ef52345eed75622d7db9
izu okafor <izunnavic@gmail.com>
2022-10-24 21:25:05 +0100

    added 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	i = 0;
+	for (n--; n >= 0; n--, i++)
 	{
-		printf(" %d", a[i]);
-		if (i != n)
-			printf(",");
+		printf("%d", a[i]);
+		if (n > 0)
+			printf(", ");
 	}
+	printf("\n");
 }
