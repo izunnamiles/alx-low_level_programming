6067ac8bce3f88de4bc6e638aef4469051aab17b
izu okafor <izunnavic@gmail.com>
2022-10-24 20:48:40 +0100

    added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - entry point
+ *
+ * Description: Pointers, array and strings
+ *
+ * @str: parameter to be an char
+ *
+ * Return: returns the string;
+ */
+void puts2(char *str);
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+		if(str[n] % 2 == 0)
+			_putchar(str[n] + '0');
+	_putchar('\n');
+}
