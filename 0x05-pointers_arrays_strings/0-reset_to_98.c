b3ee33f59bb4f1bcc5a3cb3c5071b2745549afbe
izu okafor <izunnavic@gmail.com>
2022-10-24 10:41:55 +0100

    added 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - entry point
+ *
+ * Description: Pointers, array and strings
+ *
+ * @n: parameters expects Capital
+ *
+ * Return: converts value set to the function to 98;
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
