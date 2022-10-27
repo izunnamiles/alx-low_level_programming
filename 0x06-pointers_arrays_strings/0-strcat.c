8216a15c8556207f59073ad335e1c18b07232f7d
izu okafor <izunnavic@gmail.com>
2022-10-27 01:05:17 -0700

    added 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,12 +8,10 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, n;
-
+	
 	for (i = 0; dest[i] != '\0'; i++)
-    ;
-
+		;
 	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
-    ;
-
+		;
 	return (dest);
 }
