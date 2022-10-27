aba33919161c70cdd5ad9501298ac01e85f8c87f
izu okafor <izunnavic@gmail.com>
2022-10-27 01:27:22 -0700

    added 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i, m;
 
 	for (i = 0; dest[i] != '\0'; i++)
-   ;
+		;
 	for (m = 0; m < n; m++)
 	{
 		dest[i + m] = src[m];
@@ -21,3 +21,4 @@ char *_strncat(char *dest, char *src, int n)
 
 	return (dest);
 }
+
