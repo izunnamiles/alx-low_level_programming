47f6657e314b3e589bd8c11a4e3b3b2ad2a0b5c6
izu okafor <izunnavic@gmail.com>
2022-10-27 09:24:51 +0100

    added 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: input of max bytes to be used
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, m;
+
+	for (i = 0; dest[i] != '\0'; i++)
+   ;
+	for (m = 0; m < n; m++)
+	{
+		dest[i + m] = src[m];
+		if (src[m] == '\0')
+			m = n;
+	}
+
+	return (dest);
+}
