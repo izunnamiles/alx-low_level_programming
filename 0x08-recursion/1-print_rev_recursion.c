c814d232d6b9a1f43002a6f1fd99963cf5269cf8
izu okafor <izunnavic@gmail.com>
2022-11-02 20:10:05 +0100

    added 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,10 +7,7 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
-		_putchar(*s);
-		_print_rev_recursion(s--);
-	}
+		return;
+  _print_rev_recursion(s + 1);
+  _putchar(*s);
 }
