77a9f911799cc2102e0612968c6c1dfcf2b4656e
izu okafor <izunnavic@gmail.com>
2022-11-18 08:21:08 +0100

    added 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Print a name from a function pointer
+ * @name: char string
+ * @f: function pointer that takes a string argument
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+  if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
