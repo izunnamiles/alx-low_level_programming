1425eb37d1a0ffb1f3b3d1997bbafd5fb03d0feb
izu okafor <izunnavic@gmail.com>
2022-12-02 12:47:11 +0100

    added 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,21 +1,40 @@
 #include "main.h"
+
 /**
- * clear_bit - Entry Point
- * @n: input
- * @index: index
- * Return: 0
+ * clear_bit - Set the value of a bit at a given index to 0
+ * @n: The unsigned long int to print in binary
+ * @index: The index at which to set the value to 0
+ *
+ * Return: 1 if it succeeds, -1 if it fails
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bits;
+	unsigned long int revnum;
+	unsigned long int num;
+	unsigned long int count;
+	unsigned long int oldn;
 
-	if (n == NULL)
+	revnum = count = num = 0;
+	oldn = *n;
+	if (index > 63)
 		return (-1);
-
-	bits = 1 << index;
-
-	if ((bits | *n) == *n)
-		*n = *n ^ bits;
-
+	while (count < 63)
+	{
+		revnum = revnum << 1;
+		if (count == index)
+			revnum += 0;
+		else
+			revnum += oldn & 1;
+		count++;
+		oldn = oldn >> 1;
+	}
+	while (count > 0)
+	{
+		num = num << 1;
+		num += revnum & 1;
+		revnum = revnum >> 1;
+		count--;
+	}
+	*n = num;
 	return (1);
 }
