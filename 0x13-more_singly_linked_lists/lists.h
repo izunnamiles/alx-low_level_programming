c9ab32cb3e45a5ef8fc36cc2149bd00dc63ecd24
izunna <izunnavic@gmail.com>
2022-12-31 12:59:39 +0100

    added 103-find_loop.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -29,10 +29,6 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-// size_t print_listint_safe(const listint_t *head);
-// listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address);
-// void free_listadd(listadd_t *head);
-size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 
